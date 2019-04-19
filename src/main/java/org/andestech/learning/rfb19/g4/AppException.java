6db18afa8d5526cda50a63947a84bcd9c5db8715
a0z9 <a0z9@rambler.ru>
2019-04-19 19:24:36 +0300

    Exceptions

--- src/main/java/org/andestech/learning/rfb19/g4/AppException.java
+++ src/main/java/org/andestech/learning/rfb19/g4/AppException.java
@@ -2,10 +2,86 @@ package org.andestech.learning.rfb19.g4;
 
 public class AppException {
 
+
+ private static void printStack(Exception ex)
+ {
+     System.out.println(ex );
+     for(StackTraceElement el: ex.getStackTrace())
+     {
+         System.out.println(el.getFileName() + ":" +
+                 el.getClassName() + ":" +
+                 el.getMethodName() + ":"+
+                 el.getLineNumber());
+
+
+     }
+
+
+ }
+
+  private static void testException(){
+      int[] arr = {1,2,3};
+
+
+      try {
+          arr[3] = 22;
+
+          System.out.println("End of try block");
+      }
+      catch (NullPointerException ex)
+      {
+      printStack(ex);
+      }
+
+
+  }
+
     public static void main(String[] args)
     {
 
+        try{
+        testException();}
+        catch (IndexOutOfBoundsException ex)
+        {
+            System.out.println("---------------------------");
+            printStack(ex);
 
+        }
+
+        System.out.println("+++++++++++++++++++++++++++++++");
+
+        int a=1,b=2;
+        try {
+            b = 1/(a-1);
+        }
+        catch (ClassCastException ex){
+            printStack(ex);
+        }
+        catch (Exception ex){
+            printStack(ex);
+            //----- // нельзя глотать исключения!!!!
+            throw ex;
+        }
+        finally {
+            // close ...
+            System.out.println("finally");
+
+    }
+
+    //-------------------
+
+        try{
+            //net
+            a = 1/(1-b);
+        }
+        finally {
+            // free of resources
+            System.out.println("");
+        }
+
+//....
+//....
+        System.out.println("End of main");
     }
 
 
